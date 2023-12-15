79dc803a0d1f8bb3effe2867c028231cc21329f6
eh-aque <146104968+eh-aque@users.noreply.github.com>
2023-12-15 01:22:42 -0500

    Create Project3850.py

---
+++ Project3850.py
@@ -0,0 +1,51 @@
+# Import necessary libraries
+import cv2
+import numpy as np
+import torch
+from PIL import Image
+
+# Load the image
+motherboard_img = cv2.imread('./Desktop/TMU Y4/S1/AER850 - Intro to Machine Learning/Project 3/motherboard_image.JPEG', cv2.IMREAD_COLOR)
+
+# Convert the image to grayscale
+motherboard_img_gray = cv2.cvtColor(motherboard_img, cv2.COLOR_RGB2GRAY)
+
+# Apply Gaussian blur
+motherboard_img_gray = cv2.GaussianBlur(motherboard_img_gray, (47, 47), 4)
+
+# Apply adaptive thresholding
+motherboard_img_gray = cv2.adaptiveThreshold(motherboard_img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 55, 5)
+
+# Detect edges
+edges = cv2.Canny(motherboard_img_gray, 1, 1)
+edges = cv2.dilate(edges,None, iterations = 7)
+
+# Find contours
+contours, _ = cv2.findContours(image=edges, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
+
+# Create an empty image to draw the contours
+contour_img = np.zeros_like(motherboard_img)
+
+# Draw the largest contour on the image
+cv2.drawContours(image=contour_img, contours=[max(contours, key = cv2.contourArea)], contourIdx=-1, color=(255, 255, 255), thickness=cv2.FILLED)
+
+# Bitwise-AND contour image and original image
+masked_img = cv2.bitwise_and(contour_img,  motherboard_img)
+
+print(len(contours), "objects were found in this image.")
+
+# Save the result
+cv2.imwrite('./Desktop/TMU Y4/S1/AER850 - Intro to Machine Learning/Project 3/motherboard_output.jpeg', masked_img)
+
+# # Load the YOLOv8 model
+# model = torch.hub.load('ultralytics/yolov8', 'yolov8_nano')
+
+# # Train the model
+# model.train('path_to_your_dataset', epochs=200, batch_size=16, imgsz=900)
+
+# # Evaluate the model
+# for image_path in ['image1.png', 'image2.png', 'image3.png']:
+#     img = Image.open(image_path)
+#     results = model.predict(img)
+#     results.print()  # print results to stdout
+#     results.show()  # display results
